4f3d29f6c246e1c0a96273cbcbfad9306a7b0871
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2020-09-28 11:54:23 +0300

    validate users data upon registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,21 @@
 const express = require('express');
-
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-// @route  GET api/users
+// @route  POST api/users
 // @desc   Test route
 // @access Public
-router.get('/', (req, res) => res.send('Users route...'));
+router.post(
+  '/',
+  [check('name', 'Name is required').not().isEmpty(), check('email', 'Please use a valid email').isEmail(), check('password', 'Please enter password with 6 or more xters').isLength({ min: 6 })],
+  (req, res) => {
+    //console.log(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('Users route...');
+  }
+);
 
 module.exports = router;
