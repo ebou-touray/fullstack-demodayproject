e3de6bad4ecf137f99b6c5390853b2489093d62f
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2020-09-30 10:18:55 +0300

    delete profile and user from db

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -112,4 +112,23 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
+// @route  DELETE api/profile
+// @desc   Delete profile, user and posts
+// @access Private
+
+router.delete('/', auth, async (req, res) => {
+  try {
+    //@wip delete users post
+
+    //Delete profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    //Delete user
+    await User.findOneAndRemove({ _id: req.user.id });
+    res.json({ msg: 'User deleted successfully' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
