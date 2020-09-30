a052f364ddd05d49cb5d3021ead176d74cc51749
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2020-09-30 09:19:39 +0300

    fix mongoose findandupdate error and get all profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -78,4 +78,18 @@ router.post('/', [auth, [check('status', 'Status is required').not().isEmpty(), 
   }
 });
 
+// @route  GET api/profile
+// @desc   Get all profiles
+// @access Public
+
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
