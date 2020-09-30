218341f5b4f2ae348fb5effce7df39700ecddb16
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2020-09-30 10:58:33 +0300

    added profile experience route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,6 +5,7 @@ const { check, validationResult } = require('express-validator');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const { route } = require('./users');
 
 // @route  GET api/profile/me
 // @desc   Get current user profile
@@ -130,5 +131,34 @@ router.delete('/', auth, async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+// @route  PUT api/profile
+// @desc   Add experience to profile
+// @access Private
+
+router.put(
+  '/experience',
+  [auth, [check('title', 'Title is required').not().isEmpty(), check('company', 'Company is required').not().isEmpty(), check('from', 'From date is required').not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { title, company, location, from, to, current, description } = req.body;
+    const newExp = { title, company, location, from, to, current, description };
+
+    //mongodb query
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      profile.experience.unshift(newExp);
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
