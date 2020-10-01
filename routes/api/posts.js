724183eb3b66257af7ab4d194a7e962f62f65e7a
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2020-10-01 11:26:45 +0300

    delete post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -70,4 +70,32 @@ router.get('/:id', auth, async (req, res) => {
   }
 });
 
+// @route  DELETE api/posts/:id
+// @desc   Delete a post
+// @access Private
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not Found' });
+    }
+
+    //check user deleting the post owns the post
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Not an authorized user' });
+    }
+
+    await post.remove();
+
+    res.json({ msg: 'Post successfully removed from database' });
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not Found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
