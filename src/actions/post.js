e10fa8a5564338987720173b3cb13ed1e15c8e9a
ebou-touray <60345712+ebou-touray@users.noreply.github.com>
2020-10-09 16:02:26 +0300

    Added a component to display single post

--- src/actions/post.js
+++ src/actions/post.js
@@ -1,6 +1,14 @@
 import axios from "axios";
 import { setAlert } from "./alert";
-import { GET_POST, GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST, ADD_POST } from "./types";
+import {
+  GET_POST,
+  GET_POSTS,
+  POST_ERROR,
+  UPDATE_LIKES,
+  DELETE_POST,
+  ADD_POST,
+  GET_POST
+} from "./types";
 
 // Get posts
 export const getPosts = () => async (dispatch) => {
@@ -72,24 +80,40 @@ export const deletePost = (id) => async (dispatch) => {
   }
 };
 
-
 // Add post
-export const addPost = FormData => async (dispatch) => {
-    const config = {
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }
+export const addPost = (FormData) => async (dispatch) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
 
   try {
-    const res = await axios.post('/api/posts/', FormData, config);
+    const res = await axios.post("/api/posts/", FormData, config);
 
     dispatch({
       type: ADD_POST,
-      payload: res.data
+      payload: res.data,
     });
 
     dispatch(setAlert("Post Created", "SUCCESS"));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Get post
+export const getPost = (id) => async (dispatch) => {
+  try {
+    const res = await axios.get(`/api/posts/${id}`);
+
+    dispatch({
+      type: GET_POST,
+      payload: res.data,
+    });
   } catch (err) {
     dispatch({
       type: POST_ERROR,
