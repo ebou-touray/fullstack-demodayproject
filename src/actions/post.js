2d63d38acf4d3f304bd4a9be47357d8a6ec88b9b
ebou-touray <60345712+ebou-touray@users.noreply.github.com>
2020-10-09 14:14:33 +0300

    Added like and unlike functionality to post

--- src/actions/post.js
+++ src/actions/post.js
@@ -3,7 +3,8 @@ import { setAlert } from './alert';
 import {
     GET_POST,
     GET_POSTS,
-    POST_ERROR
+    POST_ERROR,
+    UPDATE_LIKES
 } from './types';
 
 // Get posts
@@ -21,4 +22,38 @@ export const getPosts = () => async dispatch => {
       payload: { msg: err.response.statusText, status: err.response.status },
     });
     }
+};
+
+// ADD LIKE
+export const addLike = id => async (dispatch) => {
+  try {
+    const res = await axios.put(`/api/posts/like/${id}`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { id, likes: res.data}
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// REMOVE like
+export const removeLike = id=> async (dispatch) => {
+  try {
+    const res = await axios.put(`/api/posts/unlike/${id}`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { id, likes: res.data}
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
 };
