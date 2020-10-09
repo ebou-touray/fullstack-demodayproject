a7770c7dbe11b9c5db9db372cc0ec5cd44a71d76
ebou-touray <60345712+ebou-touray@users.noreply.github.com>
2020-10-09 15:25:15 +0300

    Implemented add & delete post functionalities

--- src/actions/post.js
+++ src/actions/post.js
@@ -1,37 +1,32 @@
-import axios from 'axios';
-import { setAlert } from './alert';
-import {
-    GET_POST,
-    GET_POSTS,
-    POST_ERROR,
-    UPDATE_LIKES
-} from './types';
+import axios from "axios";
+import { setAlert } from "./alert";
+import { GET_POST, GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST, ADD_POST } from "./types";
 
 // Get posts
-export const getPosts = () => async dispatch => {
-    try {
-        const res = await axios.get('/api/posts');
+export const getPosts = () => async (dispatch) => {
+  try {
+    const res = await axios.get("/api/posts");
 
-        dispatch({
-            type: GET_POSTS,
-            payload: res.data
-        });
-    } catch (err) {
-        dispatch({
+    dispatch({
+      type: GET_POSTS,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
       type: POST_ERROR,
       payload: { msg: err.response.statusText, status: err.response.status },
     });
-    }
+  }
 };
 
 // ADD LIKE
-export const addLike = id => async (dispatch) => {
+export const addLike = (id) => async (dispatch) => {
   try {
     const res = await axios.put(`/api/posts/like/${id}`);
 
     dispatch({
       type: UPDATE_LIKES,
-      payload: { id, likes: res.data}
+      payload: { id, likes: res.data },
     });
   } catch (err) {
     dispatch({
@@ -42,14 +37,59 @@ export const addLike = id => async (dispatch) => {
 };
 
 // REMOVE like
-export const removeLike = id=> async (dispatch) => {
+export const removeLike = (id) => async (dispatch) => {
   try {
     const res = await axios.put(`/api/posts/unlike/${id}`);
 
     dispatch({
       type: UPDATE_LIKES,
-      payload: { id, likes: res.data}
+      payload: { id, likes: res.data },
     });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Delete post
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    const res = await axios.delete(`/api/posts/${id}`);
+
+    dispatch({
+      type: DELETE_POST,
+      payload: id,
+    });
+
+    dispatch(setAlert("Post Removed", "SUCCESS"));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+
+// Add post
+export const addPost = FormData => async (dispatch) => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+  try {
+    const res = await axios.post('/api/posts/', FormData, config);
+
+    dispatch({
+      type: ADD_POST,
+      payload: res.data
+    });
+
+    dispatch(setAlert("Post Created", "SUCCESS"));
   } catch (err) {
     dispatch({
       type: POST_ERROR,
--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import PostItem from './PostItem';
+import PostForm from "./PostForm";
 import { getPosts } from '../../actions/post';
 
 const Posts = ({ getPosts, post: { Posts, loading} }) => {
@@ -15,10 +16,10 @@ const Posts = ({ getPosts, post: { Posts, loading} }) => {
         <p className="lead">
             <i className="fas fa-user"></i> Welcome to the community
         </p>
-        {/* PostForm */}
+      <PostForm />
         <div className="posts">
             {Posts.map(post => (
-                <postItem key={post._id} post={post} />
+                <PostItem key={post._id} post={post} />
             ))}
         </div>
         </Fragment>
